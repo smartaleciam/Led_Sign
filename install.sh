e184a4d7bd776a2ec26b9bbf90b186758e464e1b
smartaleciam <smartaleciam@users.noreply.github.com>
2023-09-30 22:38:56 +0930

    Update install.sh

--- install.sh
+++ install.sh
@@ -377,7 +377,7 @@ systemctl start sign.service
 #######################################
 # Print notice during login regarding console access
 
-cat >> /etc/motd <<EOF 
+cat <<-EOF >> /etc/motd
 [0;31m
                   [0mLED Sign Controller[0;31m
 [1m
