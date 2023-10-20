09149601ba3327f27489e0d540075cd0a493f1e5
smartaleciam <smartaleciam@users.noreply.github.com>
2023-10-20 23:36:23 +1030

    Update install.sh

--- install.sh
+++ install.sh
@@ -285,7 +285,8 @@ echo "SIGN - Cleaning up after installing packages"
 apt-get -y clean
 
 echo "SIGN - Installing PIP Modules"
-pip3 install wheel Flask flask-socketio flask_fontawesome paho-mqtt python-gsmmodem pyftpdlib mysql-connector pyserial psutil matplotlib gevent-websocket eventlet paramiko ftputil
+pip3 install wheel Flask flask-socketio flask_fontawesome paho-mqtt python-gsmmodem mysql-connector pyserial psutil matplotlib gevent-websocket ftputil
+# pip3 install eventlet pyftpdlib paramiko
 
 echo "SIGN - Configuring shellinabox to use /var/tmp"
 echo "SHELLINABOX_DATADIR=/var/tmp/" >> /etc/default/shellinabox
