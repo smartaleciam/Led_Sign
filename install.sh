374a74f76d58d3ebfc04c4e76eb8f1ae4f0c3467
smartaleciam <smartaleciam@users.noreply.github.com>
2023-12-17 21:54:05 +1030

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 # Led Trailer Sign Install Script
 SIGNBRANCH=${SIGNBRANCH:-"master"}
-SIGNIMAGEVER="2023-12-16"
+SIGNIMAGEVER="2023-12-17"
 SIGNCFGVER="1.2"
 SIGNPLATFORM="UNKNOWN"
 SIGNDIR=/opt/sign
@@ -371,27 +371,26 @@ fi
 #######################################
 # Configure log rotation
 echo "SIGN - Configuring log rotation"
-cp /opt/sign/etc/logrotate.d/* /etc/logrotate.d/
+cp --recursive /opt/sign/etc/logrotate.d/* /etc/logrotate.d/
 sed -i -e "s/#compress/compress/" /etc/logrotate.conf
 sed -i -e "s/rotate .*/rotate 2/" /etc/logrotate.conf
 #######################################
 # Move all files to correct locations
 echo "SIGN - Moving Files to Correct locations"
-cp /opt/sign/etc/systemd/system/* /etc/systemd/system/
+cp --recursive /opt/sign/etc/systemd/system/* /etc/systemd/system/
 
 cd /etc/ppp/gprs 2> /dev/null || mkdir /etc/ppp/gprs
-cp /opt/sign/etc/ppp/gprs/* /etc/ppp/gprs/
+cp --recursive /opt/sign/etc/ppp/gprs/* /etc/ppp/gprs/
 
-#cp /opt/sign/etc/chatscripts/* /etc/chatscripts/
-cp /opt/sign/etc/update-motd.d/* /etc/update-motd.d/
-cp /opt/sign/etc/logrotate.d/* /etc/logrotate.d/
+#cp --recursive /opt/sign/etc/chatscripts/* /etc/chatscripts/
+cp --recursive /opt/sign/etc/update-motd.d/* /etc/update-motd.d/
 cp /opt/sign/etc/* /etc/
 
 cd ${SIGNHOME} 2> /dev/null || mkdir ${SIGNHOME}
-cp -R /opt/sign/sign/* ${SIGNHOME}/
+cp --recursive -R /opt/sign/sign/* ${SIGNHOME}/
 
 cd /${SIGNHOME}/../sim7600g-h 2> /dev/null || sudo mkdir ${SIGNHOME}/../sim7600g-h
-cp -R /opt/sign/sim7600g-h/* ${SIGNHOME}/../sim7600g-h/
+cp --recursive -R /opt/sign/sim7600g-h/* ${SIGNHOME}/../sim7600g-h/
 
 #######################################
 echo "SIGN - Enabling System Services"
